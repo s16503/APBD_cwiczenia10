87e08d9be1a87db6e465c680ff873c48b91b9677
s16503 <s16503@pjwstk.edu.pl>
2020-05-13 13:49:32 +0200

    modele, baza, kontroler +zad2

---
+++ APBD_cwiczenia_10/Controllers/StudentsController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using APBD_cwiczenia_10.Requests;
+using APBD_cwiczenia_10.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace APBD_cwiczenia_10.Controllers
+{
+    [Route("api/students")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+
+        //private const string ConString = "Data Source=db-mssql;Initial Catalog=s16503;Integrated Security=True;";
+
+        public IConfiguration Configuration { get; set; }
+
+        private readonly IDbService _service;
+        public StudentsController(Services.IDbService service)
+        {
+            _service = service;
+        }
+
+
+        [HttpGet]
+        public IActionResult GetStudents()
+        {          
+            return Ok(_service.GetStudents());
+        }
+
+     
+
+
+        [HttpPut("{index}")]
+        public IActionResult UpdateStudnet(UpdateStudentRequest request, string index)  // aktualizacja studenta
+        {
+          
+            var student = _service.UpdateStudent(index, request);
+
+           if (student != null)
+              return Ok(student);
+
+
+
+            return NotFound("index not found");
+        }
+
+        [HttpDelete("{index}")]
+        public IActionResult DeleteStudnet(string index)  // usuwanie studenta
+        {
+
+            try
+            {
+                _service.DeleteStudent(index);
+
+            }catch(Exception exc)
+            {
+                return NotFound(exc.Message);
+            }
+
+                   
+            return Ok("usunieto studenta");
+        }
+    }
+}
---
+++ APBD_cwiczenia_10/Services/EfDbService.cs
@@ -0,0 +1,74 @@
+﻿using APBD_cwiczenia_10.Models;
+using APBD_cwiczenia_10.Requests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APBD_cwiczenia_10.Services
+{
+    public class EfDbService :IDbService
+    {
+        public s16503Context _context { get; set; }
+
+        public EfDbService(s16503Context context)
+        {
+            _context = context;
+        }
+
+        public List<Student> GetStudents()
+        {
+            return _context.Student.ToList();
+        }
+
+        public Student UpdateStudent(string index, UpdateStudentRequest request)
+        {
+           Student s = (from student in _context.Student 
+                        where student.IndexNumber == index 
+                        select student).First();
+
+
+                if(request.FirstName != null)
+                s.FirstName = request.FirstName;
+
+                if (request.LastName != null)
+                    s.LastName = request.LastName;
+
+                if (request.IdEnrollment != null)
+                    s.IdEnrollment = request.IdEnrollment;
+
+                if (request.Password != null)
+                    s.Password = request.Password;
+
+                if (request.BirthDate != null)
+                    s.BirthDate = request.BirthDate;
+
+            
+
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+               
+            }
+
+
+            return s;
+        }
+
+        public void DeleteStudent(string index)
+        {
+
+            var student = _context.Student.Find(index);
+            if (student == null)
+                throw new Exception("nie można usunąć, nie znaleziono indeksu");
+
+
+            _context.Student.Remove(student);
+            _context.SaveChanges();
+        }
+    }
+}
